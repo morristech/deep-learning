f9161f6f4a35c8e462ed79f3fb5954b1d5da1a2b
Miquel Beltran <miquelbeltran@gmail.com>
2017-05-26 18:43:39 +0200

    added base code for the live demo

--- intro-deep-learning/intro-2-neutral-network/demo.py
+++ intro-deep-learning/intro-2-neutral-network/demo.py
@@ -14,11 +14,16 @@ class NeuralNetwork():
     def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
         for iteration in xrange(number_of_training_iterations):
             output = self.predict(training_set_inputs)
+            print "%d output: %s" %(iteration, output)
             error = training_set_outputs - output
+            print "%d error: %s" %(iteration, error)
             adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+            print "%d adjustment %s" %(iteration, adjustment)
             self.synaptic_weights += adjustment
+            print "%d synaptic %s" %(iteration, self.synaptic_weights)
 
     def predict(self, inputs):
+        print "predict -> inputs %s sw %s" %(inputs, self.synaptic_weights)
         return self.__sigmoid(dot(inputs, self.synaptic_weights))
 
 
@@ -33,7 +38,7 @@ if __name__ == "__main__":
     training_set_inputs = array([[0,0,1], [1,1,1], [1,0,1], [0,1,1]])
     training_set_outputs = array([[0,1,1,0]]).T
 
-    neural_network.train(training_set_inputs, training_set_outputs, 10000)
+    neural_network.train(training_set_inputs, training_set_outputs, 2)
 
     print 'New synaptic weights:'
     print neural_network.synaptic_weights
